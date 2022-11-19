d870209912f5323026f9f2a795e969ca7bb5e8c4
Jhon Harold Hincapie Marulanda <89672206+git-devtest@users.noreply.github.com>
2022-11-18 22:06:24 -0500

    Ejercicio Calculadora, Curso Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  titulo = 'Aplicación Calculadora';
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+  sumar(): void {
+    this.resultado = this.operandoA + this.operandoB;
+  }
 }
